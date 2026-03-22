02119df34008317d9095b1ce0a215f608b61736b
Terry Akinyi <terryakinyi2001@gmail.com>
2026-03-22 09:03:27 +0300

    Initial commit: React frontend

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to Land Registry System</h2>
+      <p>Secure land ownership using blockchain</p>
+    </div>
+  );
+}
+
+export default Home;
